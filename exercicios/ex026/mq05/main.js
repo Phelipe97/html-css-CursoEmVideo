e4c0c800db300ce00d9b727101914e1c606891e4
Phelipe Santos <phe2111@hotmail.com>
2024-01-11 19:57:31 -0300

    mq5 concluido

--- exercicios/ex026/mq05/main.js
+++ exercicios/ex026/mq05/main.js
@@ -1,11 +1,20 @@
+var itens = document.getElementById('itens')
 var botao = document.getElementById('burger')
 botao.addEventListener('click', clickMenu)
 
-function clickMenu(){
-    let itens = document.getElementById('itens')
+
+
+function clickMenu(){  
     if (itens.style.display == 'block') {
         itens.style.display = 'none'
     } else {
         itens.style.display = 'block'
+    }
+}
+function mudouTamanho(){
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
     }
 }
